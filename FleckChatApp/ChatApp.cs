91263b15e5533efe78082fb9febbbebe27f9d11c
Jon <artolajon2@gmail.com>
2021-12-03 21:00:54 +0100

    Add a header with information

--- FleckChatApp/ChatApp.cs
+++ FleckChatApp/ChatApp.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FleckChatApp.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,11 +9,14 @@ namespace FleckChatApp
     {
         public bool IsServer;
         public bool IsActive = false;
+        public int Port = 0;
         private ChatServer Server;
         private ChatClient Client;
 
         public void Start(int port)
         {
+            Port = port;
+            PrintHeaderInfo();
             if (IsServer)
             {
                 Server = new ChatServer(port);
@@ -22,6 +26,25 @@ namespace FleckChatApp
                 Client = new ChatClient(port);
             }
             IsActive = true;
+            
+        }
+
+        private void PrintHeaderInfo()
+        {
+            Console.WriteLine("");
+            Console.WriteLine(@"    ________          __      ________          __     ___              ");
+            Console.WriteLine(@"   / ____/ /__  _____/ /__   / ____/ /_  ____ _/ /_   /   |  ____  ____ ");
+            Console.WriteLine(@"  / /_  / / _ \/ ___/ //_/  / /   / __ \/ __ `/ __/  / /| | / __ \/ __ \");
+            Console.WriteLine(@" / __/ / /  __/ /__/ ,<    / /___/ / / / /_/ / /_   / ___ |/ /_/ / /_/ /");
+            Console.WriteLine(@"/_/   /_/\___/\___/_/|_|   \____/_/ /_/\__,_/\__/  /_/  |_/ .___/ .___/ ");
+            Console.WriteLine(@"                                                         /_/   /_/      ");
+            Console.WriteLine("");
+            Console.WriteLine("Local chat application that is communicated through the Web Services Fleck library");
+            Console.WriteLine(IsServer ? "Server mode" : "Client mode");
+            Console.WriteLine("Port: "+Port);
+            Console.WriteLine("");
+            Console.WriteLine($"Write {Constants.ExitCommand} to close");
+            Console.WriteLine("");
         }
 
         public void Input(string text)
